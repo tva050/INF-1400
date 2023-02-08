9a06969c859e949fc4c17a1b8079b9652abc9967
tva050 <trym755@gmail.com>
2023-02-08 14:42:57 +0100

    Figures down

---
+++ Mandetory 2/boids_simulation.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+SCREEN_X = 800
+SCREEN_Y = 600
+
+BOIDS_file = "Mandetory 2\Figures\Boids.png"
+PREDATOR_file = "Mandetory 2\Figures\Predator.png"
+BG_file = "Mandetory 2\Figures\sky_bg.jpg"
+
+
+
+
+
+pg.init()
+
