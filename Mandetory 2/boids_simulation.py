4e62286c4c286dd9e94b56656c268dbbe0c34005
tva050 <trym755@gmail.com>
2023-02-12 14:40:26 +0100

    mandetory text added

--- Mandetory 2/boids_simulation.py
+++ Mandetory 2/boids_simulation.py
@@ -6,7 +6,7 @@ SCREEN_X = 800
 SCREEN_Y = 600
 BOIDS_FILE = "Mandetory 2\Figures\Boids.png"
 HOIKS_FILE = "Mandetory 2\Figures\predator.png"
-OBSTICALS_FILE = "Mandetory 2\Figures\obsticals.png"
+OBSTICALS_FILE = "Mandetory 2\Figures\obstical.png"
 BG_FILE = "Mandetory 2\Figures\sky_bg.png"
 
 pg.init()
@@ -36,6 +36,13 @@ class Drawable:
         self.move()
         screen.blit(self.img, (self.x, self.y))
 
+class moves(Drawable):
+    
+    def __init__(self, x, y, img, speed):
+        super().__init__(x, y, img)
+        self.speed = speed
+
+
 # Class for Boids which is a subclass of Drawable and has a speed variable
 class Boids(Drawable):
     
@@ -43,26 +50,41 @@ class Boids(Drawable):
         super().__init__(x, y, boids)
         self.speed = speed
     
-    def separation(self):
+    def separation(self): # Moves boids away from each other if they are too close to each other (don't crash!)
         pass
     
-    def alignment(self):
+    def alignment(self): # Moves boids in the same direction and speed as the other boids in the flock
         pass
     
-    def cohesion(self):
+    def cohesion(self): # Moves boids towards the center of the flock
+        pass
+    
+    def avoid_hoiks(self):
+        pass
+    
+    def avoid_obstacles(self):
         pass
     
 
 class Hoiks(Drawable):
-    
+    # Hoiks dont need to move in a flock, they just move in random directions, but
+    # if they crash into a boid they eat it.
     def __init__(self, x, y, speed):
         super().__init__(x, y, hoiks)
         self.speed = speed
-
-class Obstacles(Drawable):
     
+class Obstacles(Drawable):
+    # Obstacles are just static objects that the boids and hoiks can't pass through
     def __init__(self, x, y, img):
-        super().__init__(x, y, img)  
+        super().__init__(x, y, img)
+    
+    def boundaries(self):
+        pass
+    
+    def draw(self):
+        screen.blit(self.img, (self.x, self.y))  
+
+
 
 while True:
     
@@ -71,7 +93,7 @@ while True:
         break
     
     screen.blit(background, (0,0)) # første vi vil gjøre
-    
+    Obstacles(0, 0, obstacles).draw()
     pg.display.update() # oppdaterer skjermen
     
 print("GOODBYE, for now!")
