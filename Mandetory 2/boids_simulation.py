267a74ff22d96b86030a57a5d991da6c387e4219
tva050 <trym755@gmail.com>
2023-02-13 15:48:52 +0100

    algin and coh finish

--- Mandetory 2/boids_simulation.py
+++ Mandetory 2/boids_simulation.py
@@ -1,78 +1,6 @@
-""" 
-import pygame as pg
-import random
-import time
-
-SCREEN_X = 800
-SCREEN_Y = 600
-#BOIDS_FILE = r"Mandetory 2\Figures\fugu.png"
-BOID_FILE = "Mandetory 2\Figures\Boids.png"
-HOIK_FILE = "Mandetory 2\Figures\predator.png"
-OBSTICALS_FILE = "Mandetory 2\Figures\obstical.png"
-BG_FILE = "Mandetory 2\Figures\sky_bg.png"
-
-pg.init()
-
-screen = pg.display.set_mode((SCREEN_X, SCREEN_Y), 0)
-background = pg.image.load(BG_FILE)
-background = pg.transform.scale(background, (SCREEN_X, SCREEN_Y))
-background.convert()
-
-boid = pg.image.load(BOID_FILE)
-boid = pg.transform.scale(boid, (10, 10))
-hoik = pg.image.load(HOIK_FILE)
-hoik = pg.transform.scale(hoik, (10, 10))
-obstical = pg.image.load(OBSTICALS_FILE)
-obstical = pg.transform.scale(obstical, (15, 15))
-
-class Drawable:
-    def __init__(self, x, y, img):
-        self.x = x
-        self.y = y
-        self.img = img
-    
-    def move(self):
-        pass
-    
-    def draw(self):
-        self.move()
-        screen.blit(self.img, (self.x, self.y))
-
-class Boid(Drawable):
-    def __init__(self, x, y, speed):
-        super().__init__(x,y, boid)
-        self.speed_x = speed[0]
-        self.speed_y = speed[1]
-        
-    def move(self):
-        self.x = self.x + self.speed_x
-        self.y = self.y + self.speed_y
-        if self.x < 0:
-            self.speed_x = abs(self.speed_x)
-        if self.y < 0:
-            self.speed_y = abs(self.speed_y)
-        if self.x > SCREEN_X:
-            self.speed_x = -abs(self.speed_x)
-        if self.y > SCREEN_Y:
-            self.speed_y = -abs(self.speed_y)
-            
-    def update(self):
-        self.x += self.
-
-single_boid = Boid(SCREEN_X/2, SCREEN_Y/2, (2,2))
-
-while True:
-    event = pg.event.poll()
-    if event.type == pg.QUIT:
-        break
-    
-    screen.blit(background, (0,0))
-    single_boid.draw()
-    
-    pg.display.update() # oppdaterer skjermen """
-
 import pygame as pg
 from pygame import Vector2 as Vec2
+import numpy as np
 import random
 import time
 
@@ -122,9 +50,6 @@ class Moving:
         self.position += self.velocity
         
     
-    #def separtion(self):
-    """ The separation function, is used to make the boids """    
-    
     def alginment(self, boids):
         """ 
         Every boid sees the locoal boids, so the boids will try to algin with the other boids. Doing this by
@@ -141,7 +66,8 @@ class Moving:
             avg_vel /= len(local_boids) # 
             self.velocity = (self.velocity + avg_vel) / 2  
             self.velocity.scale_to_length(2) 
-          
+        return self.velocity
+    
     def cohesion(self, boids):
         """ 
         The cohesion function, is used to make the boids try to stay together, by making the boids try to go to the center of the local boids
@@ -153,7 +79,17 @@ class Moving:
         if len(local_boids) > 0:
             avg_pos = Vec2(0, 0)
             for boid in local_boids:
-                avg_pos +=
+                avg_pos += boid.position 
+            avg_pos /= len(local_boids)
+            self.velocity += (avg_pos - self.position) / 100
+            self.velocity.scale_to_length(2)
+        return self.velocity
+            
+    def separtion(self, boids):
+        """ The separation function, is used to make the boids """
+        local_boids = []
+        for boid in boids:
+            distance = np.lina
                     
             
 
@@ -172,6 +108,7 @@ class Drawable:
         for obj in objects:
             obj.draw() """
 
+
 class Boids(Moving): # In class Boids we will use the class Moves to move, for us the be availble to move something we need to first draw it
     
     def __init__(self):
@@ -181,11 +118,11 @@ class Boids(Moving): # In class Boids we will use the class Moves to move, for u
         self.move()
         self.update()
         self.alginment(boids) # This is the alginment function
+        self.cohesion(boids) # This is the cohesion function
         screen.blit(boid, (self.position.x, self.position.y))
     
     
     
-
 #class Hoiks(Moves):
 
 #class Objects(MOves):
