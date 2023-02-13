f1ac44e98804e9ea50541938d7b5019de2721bfc
tva050 <trym755@gmail.com>
2023-02-13 10:16:56 +0100

    ..A new start..

--- Mandetory 2/boids_simulation.py
+++ Mandetory 2/boids_simulation.py
@@ -1,11 +1,13 @@
+""" 
 import pygame as pg
 import random
 import time
 
 SCREEN_X = 800
 SCREEN_Y = 600
-BOIDS_FILE = "Mandetory 2\Figures\Boids.png"
-HOIKS_FILE = "Mandetory 2\Figures\predator.png"
+#BOIDS_FILE = r"Mandetory 2\Figures\fugu.png"
+BOID_FILE = "Mandetory 2\Figures\Boids.png"
+HOIK_FILE = "Mandetory 2\Figures\predator.png"
 OBSTICALS_FILE = "Mandetory 2\Figures\obstical.png"
 BG_FILE = "Mandetory 2\Figures\sky_bg.png"
 
@@ -13,17 +15,17 @@ pg.init()
 
 screen = pg.display.set_mode((SCREEN_X, SCREEN_Y), 0)
 background = pg.image.load(BG_FILE)
-background = pg.transform.scale(background, (SCREEN_X, SCREEN_Y)).convert()
+background = pg.transform.scale(background, (SCREEN_X, SCREEN_Y))
+background.convert()
 
+boid = pg.image.load(BOID_FILE)
+boid = pg.transform.scale(boid, (10, 10))
+hoik = pg.image.load(HOIK_FILE)
+hoik = pg.transform.scale(hoik, (10, 10))
+obstical = pg.image.load(OBSTICALS_FILE)
+obstical = pg.transform.scale(obstical, (15, 15))
 
-boids = pg.image.load(BOIDS_FILE)
-hoiks = pg.image.load(HOIKS_FILE)
-obstacles = pg.image.load(OBSTICALS_FILE)
-
-
-# Class for all objects that can be drawn on the screen
 class Drawable:
-    
     def __init__(self, x, y, img):
         self.x = x
         self.y = y
@@ -36,65 +38,90 @@ class Drawable:
         self.move()
         screen.blit(self.img, (self.x, self.y))
 
-class moves(Drawable):
-    
-    def __init__(self, x, y, img, speed):
-        super().__init__(x, y, img)
-        self.speed = speed
-
-
-# Class for Boids which is a subclass of Drawable and has a speed variable
-class Boids(Drawable):
-    
+class Boid(Drawable):
     def __init__(self, x, y, speed):
-        super().__init__(x, y, boids)
-        self.speed = speed
-    
-    def separation(self): # Moves boids away from each other if they are too close to each other (don't crash!)
-        pass
-    
-    def alignment(self): # Moves boids in the same direction and speed as the other boids in the flock
-        pass
-    
-    def cohesion(self): # Moves boids towards the center of the flock
-        pass
-    
-    def avoid_hoiks(self):
-        pass
-    
-    def avoid_obstacles(self):
-        pass
-    
+        super().__init__(x,y, boid)
+        self.speed_x = speed[0]
+        self.speed_y = speed[1]
+        
+    def move(self):
+        self.x = self.x + self.speed_x
+        self.y = self.y + self.speed_y
+        if self.x < 0:
+            self.speed_x = abs(self.speed_x)
+        if self.y < 0:
+            self.speed_y = abs(self.speed_y)
+        if self.x > SCREEN_X:
+            self.speed_x = -abs(self.speed_x)
+        if self.y > SCREEN_Y:
+            self.speed_y = -abs(self.speed_y)
+            
+    def update(self):
+        self.x += self.
 
-class Hoiks(Drawable):
-    # Hoiks dont need to move in a flock, they just move in random directions, but
-    # if they crash into a boid they eat it.
-    def __init__(self, x, y, speed):
-        super().__init__(x, y, hoiks)
-        self.speed = speed
-    
-class Obstacles(Drawable):
-    # Obstacles are just static objects that the boids and hoiks can't pass through
-    def __init__(self, x, y, img):
-        super().__init__(x, y, img)
-    
-    def boundaries(self):
-        pass
-    
-    def draw(self):
-        screen.blit(self.img, (self.x, self.y))  
-
-
+single_boid = Boid(SCREEN_X/2, SCREEN_Y/2, (2,2))
 
 while True:
-    
-    event = pg.event.wait()
-    if event.type == pg.QUIT: # avslutter programmet med krysset på vinduet
+    event = pg.event.poll()
+    if event.type == pg.QUIT:
         break
     
-    screen.blit(background, (0,0)) # første vi vil gjøre
-    Obstacles(0, 0, obstacles).draw()
-    pg.display.update() # oppdaterer skjermen
+    screen.blit(background, (0,0))
+    single_boid.draw()
     
-print("GOODBYE, for now!")
+    pg.display.update() # oppdaterer skjermen """
+
+import pygame as pg
+from pygame import Vector2 as Vec2
+import random
+import time
+
+SCREEN_X = 800
+SCREEN_Y = 600
+#BOIDS_FILE = r"Mandetory 2\Figures\fugu.png"
+BOID_FILE = "Mandetory 2\Figures\Boids.png"
+HOIK_FILE = "Mandetory 2\Figures\predator.png"
+OBSTICALS_FILE = "Mandetory 2\Figures\obstical.png"
+BG_FILE = "Mandetory 2\Figures\sky_bg.png"
+
+pg.init()
+
+screen = pg.display.set_mode((SCREEN_X, SCREEN_Y), 0)
+background = pg.image.load(BG_FILE)
+background = pg.transform.scale(background, (SCREEN_X, SCREEN_Y)).convert()
+
+boid = pg.image.load(BOID_FILE)
+boid = pg.transform.scale(boid, (10, 10))
+hoik = pg.image.load(HOIK_FILE)
+hoik = pg.transform.scale(hoik, (10, 10))
+obstical = pg.image.load(OBSTICALS_FILE)
+obstical = pg.transform.scale(obstical, (15, 15))
+
+# Class Moves which makes the possition, speed
+class Moves:
+    
+    def __init__(self):
+        self.position = Vec2(random.randint(800), random.randint(600))
+        self.velocity = Vec2(random.randint(0.5, 1.5), random.randint(0.5, 1.5))
+        
+    def edges(self):
+        self.position = self.position + self.velocity
+        if self.position.x < 0:
+            self.velocity.x = abs(self.velocity.x)
+        if self.position.y < 0:
+            self.velocity.x = abs(self.velocity.y)
+        
+    
+    #def separtion(self, x, y):
+    
+    #def alginment(x,y,z):
+    
+    #def cohesion:
+    
+
+#class Boids(Moves): # In class Boids we will use the class Moves to move, for us the be availble to move something we need to first draw it
+
+#class Hoiks(Moves):
+
+#class Objects(MOves):
 
