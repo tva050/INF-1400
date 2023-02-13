112b2438068a4047d39317312f5922a1040440d5
tva050 <trym755@gmail.com>
2023-02-13 13:45:28 +0100

    Alginment and cohesion functions

--- Mandetory 2/boids_simulation.py
+++ Mandetory 2/boids_simulation.py
@@ -98,30 +98,117 @@ obstical = pg.image.load(OBSTICALS_FILE)
 obstical = pg.transform.scale(obstical, (15, 15))
 
 # Class Moves which makes the possition, speed
-class Moves:
+class Moving:
     
     def __init__(self):
-        self.position = Vec2(random.randint(800), random.randint(600))
-        self.velocity = Vec2(random.randint(0.5, 1.5), random.randint(0.5, 1.5))
+        self.position = Vec2(random.randint(20, 800), random.randint(20, 600))
+        self.velocity = Vec2(random.uniform(-1, 1), random.uniform(-1, 1))
         
-    def edges(self):
+        self.perceived_distance = 100
+
+        
+    def move(self):
         self.position = self.position + self.velocity
         if self.position.x < 0:
             self.velocity.x = abs(self.velocity.x)
         if self.position.y < 0:
-            self.velocity.x = abs(self.velocity.y)
+            self.velocity.y = abs(self.velocity.y)
+        if self.position.x > SCREEN_X:
+            self.velocity.x = -abs(self.velocity.x)
+        if self.position.y > SCREEN_Y:
+            self.velocity.y = -abs(self.velocity.y)
+    
+    def update(self):
+        self.position += self.velocity
         
     
-    #def separtion(self, x, y):
+    #def separtion(self):
+    """ The separation function, is used to make the boids """    
     
-    #def alginment(x,y,z):
-    
-    #def cohesion:
-    
+    def alginment(self, boids):
+        """ 
+        Every boid sees the locoal boids, so the boids will try to algin with the other boids. Doing this by
+        taking the average of the velocity of the boids in the local area. 
+        """
+        local_boids = [] # List of boids in the local area
+        for boid in boids: 
+            if boid.position.distance_to(self.position) < self.perceived_distance: # if the boid is in the local area at a distance of 100
+                local_boids.append(boid)
+        if len(local_boids) > 0: # if there are boids in the local area
+            avg_vel = Vec2(0, 0) # average velocity
+            for boid in local_boids: # for every boid in the local area
+                avg_vel += boid.velocity # average velocity is added to the velocity of the boid
+            avg_vel /= len(local_boids) # 
+            self.velocity = (self.velocity + avg_vel) / 2  
+            self.velocity.scale_to_length(2) 
+          
+    def cohesion(self, boids):
+        """ 
+        The cohesion function, is used to make the boids try to stay together, by making the boids try to go to the center of the local boids
+        """
+        local_boids = []
+        for boid in boids:
+            if boid.position.distance_to(self.position) < self.perceived_distance:
+                local_boids.append(boid)
+        if len(local_boids) > 0:
+            avg_pos = Vec2(0, 0)
+            for boid in local_boids:
+                avg_pos +=
+                    
+            
 
-#class Boids(Moves): # In class Boids we will use the class Moves to move, for us the be availble to move something we need to first draw it
+
+""" 
+---- Trying to make a class that can draw objects ----
+class Drawable:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+    
+    def draw(self, objects):
+        self.move()
+        screen.blit(self.img, (self.x, self.y))
+        for obj in objects:
+            obj.draw() """
+
+class Boids(Moving): # In class Boids we will use the class Moves to move, for us the be availble to move something we need to first draw it
+    
+    def __init__(self):
+        super().__init__()
+    
+    def draw(self):
+        self.move()
+        self.update()
+        self.alginment(boids) # This is the alginment function
+        screen.blit(boid, (self.position.x, self.position.y))
+    
+    
+    
 
 #class Hoiks(Moves):
 
 #class Objects(MOves):
 
+def draw():
+    for boid in boids:
+        boid.draw()
+
+boids = [Boids() for _ in range(20)]
+
+
+prev_time = time.time() * 1000
+while True:
+    now = time.time() * 1000
+    if now - prev_time > 60:
+        event = pg.event.poll()
+        if event.type == pg.QUIT:
+            break
+        
+        screen.blit(background, (0,0))
+        draw()
+        
+        pg.display.update()
+        prev_time = now
+print("Good bye, for now! ")
+        
