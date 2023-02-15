12a7d0025ce6f5773287da5949239a425f57115b
tva050 <trym755@gmail.com>
2023-02-15 09:21:06 +0100

    nrw fig

--- Mandetory 2/boids_simulation.py
+++ Mandetory 2/boids_simulation.py
@@ -6,19 +6,22 @@ import time
 
 SCREEN_X = 800
 SCREEN_Y = 600
-BOID_FILE = "Mandetory 2\Figures\Boids.png"
-HOIK_FILE = "Mandetory 2\Figures\predator.png"
-OBSTICALS_FILE = "Mandetory 2\Figures\obstical.png"
-BG_FILE = "Mandetory 2\Figures\sky_bg.png"
-
+#BOID_FILE = r"Mandetory 2\Figures\right-arrow.png"
+BOID_FILE = r"Mandetory 2\Figures\navigate.png"
+HOIK_FILE = r"Mandetory 2\Figures\hoik.png"
+OBSTICALS_FILE = r"Mandetory 2\Figures\obstical.png"
+#BG_FILE = "Mandetory 2\Figures\sky_bg.png"
+#BG_FILE = r"Mandetory 2\Figures\bk.jpg"
 pg.init()
 
 screen = pg.display.set_mode((SCREEN_X, SCREEN_Y), 0)
-background = pg.image.load(BG_FILE)
-background = pg.transform.scale(background, (SCREEN_X, SCREEN_Y)).convert()
+""" background = pg.image.load(BG_FILE)
+background = pg.transform.scale(background, (SCREEN_X, SCREEN_Y))
+background.convert() """
+
 
 boid = pg.image.load(BOID_FILE)
-boid = pg.transform.scale(boid, (10, 10))
+#boid = pg.transform.scale(boid, (10, 10))
 hoik = pg.image.load(HOIK_FILE)
 hoik = pg.transform.scale(hoik, (10, 10))
 obstical = pg.image.load(OBSTICALS_FILE)
@@ -33,7 +36,6 @@ class Moving:
         self.velocity.normalize()
         
         self.perceived_distance = 100
-        self.flee_radius = 40
         
     def move(self):
         self.position = self.position + self.velocity
@@ -71,22 +73,23 @@ class Moving:
     
     def cohesion(self, boids):
         """ 
-        The cohesion function, is used to make the boids try to stay together, by making the boids try to go to the center of the local boids
+        The cohesion function, is used to make the boids try to stay together, 
+        by making the boids try to go to the center of the local boids (the gruop of boids)
         """
-        local_boids = []
-        for boid in boids:
-            if boid.position.distance_to(self.position) < self.perceived_distance:
-                local_boids.append(boid)
+        local_boids = [] # List of boids in the local area
+        for boid in boids: 
+            if boid.position.distance_to(self.position) < self.perceived_distance: 
+                local_boids.append(boid) # if the boid distance is smaller than the perceived distance append to the local boids
         if len(local_boids) > 0:
             avg_pos = Vec2(0, 0)
-            for boid in local_boids:
-                avg_pos += boid.position 
+            for boid in local_boids: 
+                avg_pos += boid.position # updating the average position by the boid position
             avg_pos /= len(local_boids)
-            self.velocity += (avg_pos - self.position) / 100
+            self.velocity += (avg_pos - self.position) / self.perceived_distance
             self.velocity.scale_to_length(2)
         return self.velocity
             
-    """ def separtion(self, boids):
+    """ def separation(self, boids):
         #The separation function, is used to make the boids 
         total = 0
         local_boids = []
@@ -106,9 +109,9 @@ class Moving:
             steer.scale_to_length(1)
             self.velocity += steer
             self.velocity.scale_to_length(2)
-        return self.velocity  """  
+        return self.velocity """   
     
-    def separtion(self, boids):
+    def separation(self, boids):
         local_boids = []
         for boid in boids:
             distance = boid.position.distance_to(self.position)
@@ -128,12 +131,8 @@ class Moving:
             self.velocity.scale_to_length(2)
         return self.velocity
     
-    def boid_avoid_hoik(self, object):
-        distance = object.position.distance_to(self.position)
-        if distance < self.flee_radius:
-            self.velocity += (self.position - object.position) / 100
-            self.velocity.scale_to_length(2)
-        return self.velocity
+
+        
              
 """ 
 ---- Trying to make a class that can draw objects ----
@@ -157,10 +156,17 @@ class Boids(Moving): # In class Boids we will use the class Moves to move, for u
     
     # Function wich rotate the triangle (boid) to the direction of the velocity
     def rotate(self):
-        angle = math.degrees(math.atan2(self.velocity.y, self.velocity.x)) - 90
+        angle = math.degrees(-math.atan2(self.velocity.y, self.velocity.x)) - 45
         rotated_image = pg.transform.rotate(boid, angle) # Rotates the boid to the direction of the velocity vector 
         new_rect = rotated_image.get_rect(center=boid.get_rect(topleft=(self.position.x, self.position.y)).center)
         return rotated_image, new_rect
+    
+    def avoid_hoiks(self):
+        for hoik in hoiks:
+            distance = hoik.position.distance_to(self.position)
+            if distance < 100:
+                self.velocity += (self.position - hoik.position) / 100
+                self.velocity.scale_to_length(2)
     
     # Function which draws the boid and the behaviour of the boid
     def draw_and_behaviour(self):
@@ -169,8 +175,8 @@ class Boids(Moving): # In class Boids we will use the class Moves to move, for u
         self.update()
         self.alginment(boids) # This is the alginment function
         self.cohesion(boids) # This is the cohesion function
-        self.separtion(boids) # This is the separtion function
-        self.boid_avoid_hoik(hoiks) # This is the boid avoid hoik function
+        self.separation(boids) # This is the separtion function
+        self.avoid_hoiks()
         
         screen.blit(boid, (self.position.x, self.position.y))
         
@@ -178,8 +184,8 @@ class Boids(Moving): # In class Boids we will use the class Moves to move, for u
 class Hoiks(Moving):
     def __init__(self):
         super().__init__()
-        
-        
+            
+
     def draw_and_behaviour(self):
         self.move()
         self.update()
@@ -197,7 +203,7 @@ def draw():
         
 
 boids = [Boids() for _ in range(20)]
-hoiks = [Hoiks() for _ in range(5)]
+hoiks = [Hoiks() for _ in range(10)]
 
 prev_time = time.time() * 1000
 while True:
@@ -207,7 +213,8 @@ while True:
         if event.type == pg.QUIT:
             break
         
-        screen.blit(background, (0,0))
+        #screen.blit(background, (0,0))
+        screen.fill([32,32,32])
         
         draw()
         
