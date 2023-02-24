cd1b8d8aa75efad946ae933cbd8eaca420e24823
tva050 <trym755@gmail.com>
2023-02-24 10:51:45 +0100

    Forelesning

--- Forlesning/Forelesning 10/kolektivtransport.py
+++ Forlesning/Forelesning 10/kolektivtransport.py
@@ -22,9 +22,9 @@ class Person():
     
 class Båtfører(Person):
 
-    def __init__(self, navn, ansvarsområde, serifikat, kan_sikkerhet):
+    def __init__(self, navn, ansvarsområde, sertifikat, kan_sikkerhet):
         super().__init__(navn, ansvarsområde)
-        self.serifikat = serifikat
+        self.serifikat = sertifikat
         self.kan_sikkerhet = kan_sikkerhet
     
     # Polygrafi
@@ -37,13 +37,24 @@ class Båtfører(Person):
             s += "\nPersonen har IKKE sikkerhetskurs"
         return s
 
-class Busssjåfør(Person):
+class Bussjåfør(Person):
     
-    def __init__(self, navn, serifikat):
+    def __init__(self, navn, sertifikat):
         super().__init__(navn, "Sjåfør")
-        
-            
+        self.sertifikat = sertifikat
+    
+    def endreAnsvar(self, ansvar):
+        raise RuntimeError("Bussjåfør kan ikke endre ansvar")
+    
+    def __str__(self):
+        s = "=== Bussjåfør ===\n"
+        s += super().__str__()
+        return s
 
 if __name__ == "__main__":
     per = Båtfører("per", "kaptein", "A5L1", True)
+    anne = Bussjåfør("Anne", "D1E")
+    
     print(per)
+    print()
+    print(anne)
