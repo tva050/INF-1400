f211d1c86930fdde947575de968e2c74d523fbfb
tva050 <trym755@gmail.com>
2023-04-11 13:24:32 +0200

    Finish

--- Mandetory 3/start_game.py
+++ Mandetory 3/start_game.py
@@ -5,11 +5,13 @@ module:: start_game.py
     This module contains the if __name__ == "__main__": statement. to start the game.
 """
 from screens import Screens
+import cProfile
 
 
 if __name__ == "__main__": 
     """ 
     Starts the game.
     """
+    cProfile.run('start = Screens.main_manu(Screens)')
     start = Screens.main_manu(Screens)
     
